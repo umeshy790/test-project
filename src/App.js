90f223b70f36254288c68165fd8006d327475133
Umesh Yadav <umeshyadav@Umeshs-MacBook-Air.local>
2021-10-13 16:08:35 +0530

    environment variables 5

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is env secret {process.env.REACT_APP_FIREBASE_ID}</p>
+        <p>This should come from env . {process.env.REACT_APP_FIREBASE_ID}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
